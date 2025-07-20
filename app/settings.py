4d3e83bab0b1885defbe63dcf3620e5e359707f5
GetApple <getapple@GetApples-MacBook-Pro.local>
2025-07-20 20:38:21 +0300

    prodfinal

--- app/settings.py
+++ app/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["ionex.onrender.com", 'localhost', '127.0.0.1', 'ionex.com.ua']
+ALLOWED_HOSTS = ["ionex.onrender.com", 'localhost', '127.0.0.1', 'ionex.com.ua', 'www.ionex.com.ua']
 
 
 # Application definition
