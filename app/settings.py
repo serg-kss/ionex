7485454a3b617e0040db2f18278fb74e1ef8c0cd
GetApple <getapple@GetApples-MacBook-Pro.local>
2025-07-20 19:54:09 +0300

    prod3

--- app/settings.py
+++ app/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*", 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["ionex.onrender.com", 'localhost', '127.0.0.1', 'ionex.com.ua']
 
 
 # Application definition
